b037f712834d12fe7c7f4539b81179f102e2ce03
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-11 00:56:15 +0300

    Practice Question 3 Done

--- app.js
+++ app.js
@@ -6,5 +6,15 @@ app.use(express.json());
 
 const todos = [
     { todo : "wake up", isCompleted: false},
-    {todo : "Eat Breakfast", isCompleted:false}
+    {todo : "Eat Breakfast", isCompleted: false}
 ];
+
+app.get("/todos" , (req , res) => {
+    res.status(200);
+    res.json(todos);
+});
+
+
+app.listen(port , ()=> {
+    console.log(`Practice app listening at http://localhost:${port}`)
+})
