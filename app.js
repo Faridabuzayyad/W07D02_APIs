73e3576462c61f9a74531e1cc8c0813a345c2457
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-11 18:58:17 +0300

    Practice Question 5 - Done

--- app.js
+++ app.js
@@ -20,6 +20,24 @@ app.post("/create/todo" , (req , res) => {
     res.json(todos);
 })
 
+app.put("/update/todo/:name" , (req , res) => {
+    const updatedTodo = req.params.name;
+    let i;
+    const match = todos.find((element , index)=>{
+        i = index;
+        return element.todo === updatedTodo
+    });
+    if(match){
+        todos[i]= req.body;
+        res.status(200);
+        res.send(todos[i]);
+    }
+    else{
+        res.status(404);
+        res.send("No Match found")
+    }
+});
+
 app.listen(port , ()=> {
     console.log(`Practice app listening at http://localhost:${port}`)
 })
